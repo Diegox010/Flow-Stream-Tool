d91a7cd61bb8d5343b57453a1a1b11c4ba751e96
Oscar Alfonso Guerrero Nuñez <oaguerrero.95@gmail.com>
2022-11-23 12:37:54 -0600

    Added static results screen

---
+++ results/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const testJSON = [
+    {
+      tag: 'Tag',
+      team: 'Team',
+      character: 'Mr Game & Watch',
+      pose: 'Default Right',
+    },
+  ];
+
+  console.log('running');
+});

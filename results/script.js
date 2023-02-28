697b1cd643098c602d3be6613474c3e2d83fbbb6
Oscar Alfonso Guerrero Nuñez <oaguerrero.95@gmail.com>
2023-02-28 14:48:10 -0600

    index.html now loads data from json

--- results/script.js
+++ results/script.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const testJSON = [
-    {
-      tag: 'Tag',
-      team: 'Team',
-      character: 'Mr Game & Watch',
-      pose: 'Default Right',
-    },
-  ];
+  async function getPlayers() {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'players.json');
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          const players = JSON.parse(xhr.responseText);
+          resolve(players);
+        } else {
+          reject(xhr.statusText);
+        }
+      };
+      xhr.onerror = () => reject('Error fetching players.json');
+      xhr.send();
+    });
+  }
 
-  console.log('running');
+  getPlayers()
+    .then((players) => {
+      console.log(players);
+      players.map((player, index) => {
+        const playerElement = document.getElementById(`top-${index + 1}`);
+        playerElement.style.backgroundImage = `url('${player.url}')`;
+        playerElement.nextElementSibling.children[0].innerHTML = player.player;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
